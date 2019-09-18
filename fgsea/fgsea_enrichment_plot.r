057e291c0d1e1aa165e652e489e4bedf6ca49ad5
kujin <kkwon@unist.ac.kr>
2019-09-18 15:46:43 +0900

    Add seed to sustain reproducibility

--- fgsea/fgsea_enrichment_plot.r
+++ fgsea/fgsea_enrichment_plot.r
@@ -5,6 +5,7 @@ ranks = read.table("DAY8_vs_TPP-GA_DAY8.entrez.rnk",header= TRUE, colClasses = c
 ranks = ranks[!duplicated(ranks$geneID),]
 ranks = setNames(ranks$log2FC,ranks$geneID)
 pathways = gmtPathways("BP_mouse_gsea.c5.gmt")
+set.seed(123)
 fgseaRes <- fgsea(pathways = pathways, stats = ranks,minSize=15, maxSize=500,nperm=10000)
 GSEA_pl = plotEnrichment(pathways[["GO_WHITE_FAT_CELL_DIFFERENTIATION"]], ranks) + labs(title="GO_GO_WHITE_FAT_CELL_DIFFERENTIATION") + 
   theme(plot.title = element_text(size = 20, face = "bold",hjust = 0.5), axis.title.x = element_text(size = 16), 
