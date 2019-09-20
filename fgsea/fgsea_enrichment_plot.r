4f574cb4e808b0f6b0bdc8d4c4af829f0606d7e9
kujin <kkwon@unist.ac.kr>
2019-09-20 23:18:25 +0900

    Add Enrichment plot option based on ggplot2

--- fgsea/fgsea_enrichment_plot.r
+++ fgsea/fgsea_enrichment_plot.r
@@ -1,15 +1,16 @@
 library(data.table)
 library(fgsea)
-library(ggplot2)
+library(extrafont)
+loadfonts()
 ranks = read.table("DAY8_vs_TPP-GA_DAY8.entrez.rnk",header= TRUE, colClasses = c("character","numeric"))
 ranks = ranks[!duplicated(ranks$geneID),]
 ranks = setNames(ranks$log2FC,ranks$geneID)
 pathways = gmtPathways("BP_mouse_gsea.c5.gmt")
 set.seed(123)
 fgseaRes <- fgsea(pathways = pathways, stats = ranks,minSize=15, maxSize=500,nperm=10000)
-GSEA_pl = plotEnrichment(pathways[["GO_WHITE_FAT_CELL_DIFFERENTIATION"]], ranks) + labs(title="GO_GO_WHITE_FAT_CELL_DIFFERENTIATION") + 
-  theme(plot.title = element_text(size = 20, face = "bold",hjust = 0.5), axis.title.x = element_text(size = 16), 
-          axis.title.y = element_text(size = 16))
+GSEA_pl = plotEnrichment(pathways[["GO_WHITE_FAT_CELL_DIFFERENTIATION"]], ranks, ticksSize = 0.5) + labs(title="GO_WHITE_FAT_CELL_DIFFERENTIATION") + 
+  theme(plot.title = element_text(size = 20, face = "bold",hjust = 0.5), axis.title.x = element_text(size = 17), 
+        axis.title.y = element_text(size = 17), axis.text.x = element_text(size = 14), axis.text.y = element_text(size = 14))+ geom_line(size = 1.5, color = "green")
 pdf(file = "GSEA_white_fat_cell_differentiation", height = 8, width = 11, family = "Arial")
 op = par(mar = c(5, 4, 0.05, 0.05) + 0.1)
 plot(GSEA_pl)
